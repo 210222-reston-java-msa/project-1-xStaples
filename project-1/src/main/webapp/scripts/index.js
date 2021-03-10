e30ef33b47f3659a2706fe26f1829fd11a1e95a1
xStaples <daltonspencer112@gmail.com>
2021-03-09 18:37:07 -0800

    filenaming upd -- js func

---
+++ project-1/src/main/webapp/scripts/index.js
@@ -0,0 +1,14 @@
+const userLogin = () => {
+    console.log("Function test");
+
+    let username = document.getElementById("UNInput").value;
+    let password = document.getElementById("PWInput").value;
+
+    let loginInput = {
+        username: username,
+        password: password
+    }
+
+
+
+}
