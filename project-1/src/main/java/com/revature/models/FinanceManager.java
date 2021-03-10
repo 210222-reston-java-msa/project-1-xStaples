69a7f22e3878f536610fe7ca9a192a2d5328cf34
xStaples <daltonspencer112@gmail.com>
2021-03-09 18:25:19 -0800

    first commit, added models login page

---
+++ project-1/src/main/java/com/revature/models/FinanceManager.java
@@ -0,0 +1,10 @@
+package com.revature.models;
+
+public class FinanceManager {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    
+}
